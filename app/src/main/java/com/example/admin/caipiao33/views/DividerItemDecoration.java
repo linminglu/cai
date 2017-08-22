ec18e56f32cc675b4eb0385f0fb528209c5a7fcc
shaodong <liu799200721@gmail.com>
2017-08-22 17:06:21 +0800

    修改decoration最后一条线不绘制

--- app/src/main/java/com/example/admin/caipiao33/views/DividerItemDecoration.java
+++ app/src/main/java/com/example/admin/caipiao33/views/DividerItemDecoration.java
@@ -63,6 +63,9 @@ public class DividerItemDecoration extends RecyclerView.ItemDecoration
         final int childCount = parent.getChildCount();
         for (int i = 0; i < childCount; i++)
         {
+            if (i == childCount - 1) {
+                return;
+            }
             final View child = parent.getChildAt(i);
             final RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
             final int top = child.getBottom() + params.bottomMargin;
