ac684aa5ef08e3e1f225e333528b716e719f35a0
caoxiangyu <414262549@qq.com>
2017-10-18 10:40:12 +0800

    修改了部分BUG

--- app/src/main/java/com/example/admin/caipiao33/ChangeTiKuanPasswordActivity.java
+++ app/src/main/java/com/example/admin/caipiao33/ChangeTiKuanPasswordActivity.java
@@ -7,7 +7,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.example.admin.caipiao33.contract.IChangeTiKuanPasswordContract;
 import com.example.admin.caipiao33.presenter.ChangeTiKuanPasswordPresenter;
 import com.example.admin.caipiao33.utils.Constants;
@@ -27,15 +29,25 @@ public class ChangeTiKuanPasswordActivity extends ToolbarActivity implements Too
     EditText changetikuanpasswordNpasswordEt;
     @BindView(R.id.btn_changetikuanpassword)
     Button btnChangetikuanpassword;
+    @BindView(R.id.changetikuanpassword_npassword_tv1)
+    TextView changetikuanpasswordNpasswordTv1;
+    @BindView(R.id.changetikuanpassword_npassword_tv2)
+    TextView changetikuanpasswordNpasswordTv2;
+    @BindView(R.id.changetikuanpassword_npassword_tv3)
+    TextView changetikuanpasswordNpasswordTv3;
+    @BindView(R.id.changetikuanpassword_npassword_tv4)
+    TextView changetikuanpasswordNpasswordTv4;
     private IChangeTiKuanPasswordContract.Presenter mPresenter;
     private int haspassword;
     private int fromtikuan = 0;
+    private String[] passwordnums;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_changetikuanpassword);
+        passwordnums = getResources().getStringArray(R.array.nums);
         fromtikuan = getIntent().getIntExtra("fromtikuan", 0);
         ButterKnife.bind(this);
         mPresenter = new ChangeTiKuanPasswordPresenter(this, null);
@@ -89,26 +101,119 @@ public class ChangeTiKuanPasswordActivity extends ToolbarActivity implements Too
 
     }
 
-    @OnClick(R.id.btn_changetikuanpassword)
+    @OnClick({R.id.btn_changetikuanpassword, R.id.changetikuanpassword_npassword_tv1, R.id.changetikuanpassword_npassword_tv2, R.id.changetikuanpassword_npassword_tv3, R.id.changetikuanpassword_npassword_tv4})
     public void onViewClicked(View view)
     {
         switch (view.getId())
         {
             case R.id.btn_changetikuanpassword:
+                String npw = changetikuanpasswordNpasswordTv1.getText()
+                        .toString() + changetikuanpasswordNpasswordTv2.getText()
+                        .toString() + changetikuanpasswordNpasswordTv3.getText()
+                        .toString() + changetikuanpasswordNpasswordTv4.getText().toString();
+                //                if (StringUtils.isEmpty2(changetikuanpasswordOpasswordEt.getText().toString()))
+                //                {
+                //                    ToastUtil.show("请输入旧密码！");
+                //                    return;
+                //                }
+                //                if (StringUtils.isEmpty2(changetikuanpasswordNpasswordEt.getText().toString()))
+                //                {
+                //                    ToastUtil.show("请输入新密码！");
+                //                    return;
+                //                }
                 if (StringUtils.isEmpty2(changetikuanpasswordOpasswordEt.getText().toString()))
                 {
                     ToastUtil.show("请输入旧密码！");
                     return;
                 }
-                if (StringUtils.isEmpty2(changetikuanpasswordNpasswordEt.getText().toString()))
+                if (npw.length() < 4)
                 {
-                    ToastUtil.show("请输入新密码！");
+                    ToastUtil.show("请输入完整的新密码！");
                     return;
                 }
                 mPresenter.changeTiKuanPassword(changetikuanpasswordOpasswordEt.getText()
-                        .toString(), changetikuanpasswordNpasswordEt.getText().toString());
+                        .toString(), npw);
+                break;
+            case R.id.changetikuanpassword_npassword_tv1:
+                if (StringUtils.isEmpty2(changetikuanpasswordNpasswordTv1.getText().toString()))
+                {
+                    showOptionsDialog(-1, 1);
+                }
+                else
+                {
+                    showOptionsDialog(Integer.valueOf(changetikuanpasswordNpasswordTv1.getText()
+                            .toString()), 1);
+                }
+                break;
+            case R.id.changetikuanpassword_npassword_tv2:
+                if (StringUtils.isEmpty2(changetikuanpasswordNpasswordTv2.getText().toString()))
+                {
+                    showOptionsDialog(-1, 2);
+                }
+                else
+                {
+                    showOptionsDialog(Integer.valueOf(changetikuanpasswordNpasswordTv2.getText()
+                            .toString()), 2);
+                }
+                break;
+            case R.id.changetikuanpassword_npassword_tv3:
+                if (StringUtils.isEmpty2(changetikuanpasswordNpasswordTv3.getText().toString()))
+                {
+                    showOptionsDialog(-1, 3);
+                }
+                else
+                {
+                    showOptionsDialog(Integer.valueOf(changetikuanpasswordNpasswordTv3.getText()
+                            .toString()), 3);
+                }
+                break;
+            case R.id.changetikuanpassword_npassword_tv4:
+                if (StringUtils.isEmpty2(changetikuanpasswordNpasswordTv4.getText().toString()))
+                {
+                    showOptionsDialog(-1, 4);
+                }
+                else
+                {
+                    showOptionsDialog(Integer.valueOf(changetikuanpasswordNpasswordTv4.getText()
+                            .toString()), 4);
+                }
                 break;
         }
+    }
+
+    private void showOptionsDialog(int weizhi, final int mwhich)
+    {
+        new MaterialDialog.Builder(ChangeTiKuanPasswordActivity.this).title("请选择")
+                .items(passwordnums)
+                .positiveText(R.string.dialog_ok)
+                .negativeText(R.string.dialog_cancel)
+                .itemsCallbackSingleChoice(weizhi, new MaterialDialog.ListCallbackSingleChoice()
+                {
+                    @Override
+                    public boolean onSelection(MaterialDialog dialog, View view, int which, CharSequence text)
+                    {
+                        if (which != -1)
+                        {
+                            switch (mwhich)
+                            {
+                                case 1:
+                                    changetikuanpasswordNpasswordTv1.setText(which + "");
+                                    break;
+                                case 2:
+                                    changetikuanpasswordNpasswordTv2.setText(which + "");
+                                    break;
+                                case 3:
+                                    changetikuanpasswordNpasswordTv3.setText(which + "");
+                                    break;
+                                case 4:
+                                    changetikuanpasswordNpasswordTv4.setText(which + "");
+                                    break;
+                            }
+                        }
+                        return true;
+                    }
+                })
+                .show();
     }
 }
 
--- app/src/main/java/com/example/admin/caipiao33/TuiJianActivity.java
+++ app/src/main/java/com/example/admin/caipiao33/TuiJianActivity.java
@@ -117,10 +117,10 @@ public class TuiJianActivity extends ToolbarActivity implements Toolbar.OnMenuIt
             KLog.e(e);
         }
         tvTuijianId.setText(id + 100000 + "");
-        tvTuijianUrl.setText(HttpUtil.mNewUrl + "/common/register?tj=" + (id + 100000));
+        tvTuijianUrl.setText("https://cp89003.com" + "/common/register?tj=" + (id + 100000));
         tvTuijianShouyi.setText("¥" + result.getMySpread().getSpreadIn());
-        tvTuijianShuoming.setText("说明：每天的7点更新收益，如3号7点，会计算2号0点-24点之间的所以胡数据，然后增加您的收益。您的收益=推荐会员的有效投注额度总和÷100 x " + result
-                .getSpread() + "（转换率），小输部分四舍五入！（风险账号不参与收益计算）");
+        tvTuijianShuoming.setText("说明：每天的7点更新收益，如3号7点，会计算2号0点-24点之间的所有数据，然后增加您的收益。您的收益=推荐会员的有效投注额度总和÷100 x " + result
+                .getSpread() + "（转换率），小数部分四舍五入！（风险账号不参与收益计算）");
         tvTuijianHuiYuannum.setText("我的推荐会员（" + result.getMySpread().getSpreadCount() + "）");
         StringBuffer buffer = new StringBuffer();
         for (int i = 0; i < result.getMySpread().getSpreadMember().size(); i++)
