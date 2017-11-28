ab3cc29caa890441902204c020a4f2e12b002dbd
CXY <414262549@qq.com>
2017-11-28 17:16:19 +0800

    修改了部分充值上的问题

--- app/src/main/java/com/example/admin/caipiao33/topupactivity/WeiXinBankActivity.java
+++ app/src/main/java/com/example/admin/caipiao33/topupactivity/WeiXinBankActivity.java
@@ -241,7 +241,6 @@ public class WeiXinBankActivity extends ToolbarActivity implements Toolbar.OnMen
                 ToastUtil.show("提交成功，请稍后查询充值记录！");
                 finish();
                 EventBus.getDefault().post(new TopupEvent(""));
-
             }
 
             @Override
